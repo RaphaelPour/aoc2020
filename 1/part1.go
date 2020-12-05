34e365e52a17706661d34ed3f08e3edd90f59578
Raphael Pour <info@raphaelpour.de>
2020-12-05 16:26:28 +0100

    1: Refacotring with int loader

--- 1/part1.go
+++ 1/part1.go
@@ -2,32 +2,18 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/RaphaelPour/aoc2020/util"
 )
 
 func main() {
-	ex := make([]int, 0)
-
-	lines := util.LoadDefault()
-
-	for i, line := range lines {
-		n, err := strconv.Atoi(line)
-		if err != nil {
-			fmt.Printf("%s is not a number in line %d!\n", line, i)
-			return
-		}
-
-		ex = append(ex, n)
-
-	}
+	ex := util.LoadDefaultInt()
 
 	for i := 0; i < len(ex); i++ {
 		for j := i + 1; j < len(ex); j++ {
 			if ex[i]+ex[j] == 2020 {
-				fmt.Printf("Found %d + %d = 2020\n", ex[i], ex[j])
-				fmt.Printf("%d x %d = %d\n", ex[i], ex[j], ex[i]*ex[j])
+				fmt.Println( ex[i]*ex[j])
+				return 
 			}
 		}
 	}
--- 1/part2.go
+++ 1/part2.go
@@ -1,36 +1,18 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
+	"github.com/RaphaelPour/aoc2020/util"
 )
 
 func main() {
-	ex := make([]int, 0)
-	scanner := bufio.NewScanner(os.Stdin)
-
-	index := 0
-	for scanner.Scan() {
-		line := scanner.Text()
-		n, err := strconv.Atoi(line)
-		if err != nil {
-			fmt.Printf("%s is not a number in line %d!\n", line, index)
-			return
-		}
-
-		ex = append(ex, n)
-
-		index++
-	}
+	ex := util.LoadDefaultInt()
 
 	for i := 0; i < len(ex); i++ {
 		for j := i + 1; j < len(ex); j++ {
 			for k := j + 1; k < len(ex); k++ {
 				if ex[i]+ex[j]+ex[k] == 2020 {
-					fmt.Printf("Found %d + %d + %d = 2020\n", ex[i], ex[j], ex[k])
-					fmt.Printf("%d x %d x %d = %d\n", ex[i], ex[j], ex[k], ex[i]*ex[j]*ex[k])
+					fmt.Println( ex[i]*ex[j]*ex[k])
 				}
 			}
 		}
