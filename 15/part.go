1e844e7cf5776b9cd0ff8981ba52f68f7a61609b
Raphael Pour <info@raphaelpour.de>
2020-12-15 10:34:08 +0100

    15: Solve second part

--- 15/part.go
+++ 15/part.go
@@ -9,14 +9,14 @@ import (
 )
 
 type Spoken struct {
-	turn, preTurn uint64
+	turn, preTurn int
 }
 
 var (
 	goodColor = color.New(color.FgGreen, color.Bold)
 	badColor  = color.New(color.FgRed, color.Bold)
 	inputFile = "input"
-	solutions = map[string][]uint64{
+	solutions = map[string][]int{
 		"input":  {496, 883},
 		"input2": {436, 175594},
 		"input3": {1, 2578},
@@ -30,36 +30,30 @@ var (
 )
 
 func main() {
-	history1 := make(map[uint64]Spoken, 0)
-	history2 := make(map[uint64]Spoken, 0)
+	history1 := make(map[int]Spoken, 0)
+	history2 := make(map[int]Spoken, 0)
 	content := util.LoadString(inputFile)[0]
 	for i, el := range strings.Split(content, ",") {
-		num, err := strconv.ParseUint(el, 10, 64)
+		num, err := strconv.Atoi(el)
 		if err != nil {
 			fmt.Println(el, "is not a number in at position", i)
 			return
 		}
-		history1[num] = Spoken{turn: uint64(i + 1), preTurn: 0}
-		history2[num] = Spoken{turn: uint64(i + 1), preTurn: 0}
+		history1[num] = Spoken{turn: int(i + 1), preTurn: 0}
+		history2[num] = Spoken{turn: int(i + 1), preTurn: 0}
 	}
 
-	result := play(2020, history1)
-	fmt.Println("Invalid: 768, 656")
-	fmt.Println("part1: >>", result, "<<")
-	checkSolution(result, 1)
+	checkSolution(play(2020, history1), 1)
+	checkSolution(play(30000000, history2), 2)
 
-	fmt.Println("Too high: 2002")
-	result = play(30000000, history2)
-	fmt.Println("part2: >>", result, "<<")
-	checkSolution(result, 2)
 	return
 
 }
 
-func play(rounds uint64, history map[uint64]Spoken) uint64 {
+func play(rounds int, history map[int]Spoken) int {
 
-	next := uint64(0)
-	for turn := uint64(len(history) + 1); turn < rounds; turn++ {
+	next := int(0)
+	for turn := int(len(history) + 1); turn < rounds; turn++ {
 		if _, ok := history[next]; !ok {
 			history[next] = Spoken{turn: turn, preTurn: 0}
 			next = 0
@@ -75,13 +69,18 @@ func play(rounds uint64, history map[uint64]Spoken) uint64 {
 	return next
 }
 
-func checkSolution(result uint64, part int) {
+func checkSolution(result int, part int) {
+
+	fmt.Printf("%d: ", part)
+
 	solution, ok := solutions[inputFile]
 	if ok {
 		if solution[part-1] == result {
-			goodColor.Println("OK")
+			goodColor.Println(result)
 		} else {
-			badColor.Println("NOT OK")
+			badColor.Println(result)
 		}
+	} else {
+		fmt.Println(result)
 	}
 }
