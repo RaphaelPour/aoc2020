6b66596f4872f64a4d670b164a31ad53a1d4e658
Raphael Pour <info@raphaelpour.de>
2020-12-15 10:33:34 +0100

    13: Solve second part

--- 13/part2.go
+++ 13/part2.go
@@ -3,45 +3,59 @@ package main
 import (
 	"fmt"
 	"github.com/RaphaelPour/aoc2020/util"
-	"golang.org/x/sync/semaphore"
-	"runtime"
+	"github.com/fatih/color"
+	"math/big"
 	"strconv"
 	"strings"
-	"sync"
-	"time"
 )
 
 /*
- * Brute force:
- *
- * Check for each time step if the busses are
- * arriving in order.
- *
- * This version can check those steps in parallel.
- * The time can be incremented by the minimum
- * of the input.
- *
- * This solution takes a lot of time.
  */
 
 var (
-	mtx       sync.Mutex
-	count     = 0
-	doneCount = 0
+	goodColor = color.New(color.FgGreen, color.Bold)
+	badColor  = color.New(color.FgRed, color.Bold)
+	inputFile = "input"
+	solutions = map[string]uint64{
+		"input2": 1068781,
+		"input3": 3417,
+		"input4": 754018,
+		"input5": 779210,
+		"input6": 1261476,
+		"input7": 1202161486,
+	}
+	one = big.NewInt(1)
 )
 
-type Departure struct {
-	Index uint64
-	BusID uint64
+/* https://rosettacode.org/wiki/Chinese_remainder_theorem#Go */
+func crt(a, n []*big.Int) (*big.Int, error) {
+	p := new(big.Int).Set(n[0])
+	for _, n1 := range n[1:] {
+		p.Mul(p, n1)
+	}
+	var x, q, s, z big.Int
+	for i, n1 := range n {
+		q.Div(p, n1)
+		z.GCD(nil, &s, n1, &q)
+		if z.Cmp(one) != 0 {
+			return nil, fmt.Errorf("%d not coprime", n1)
+		}
+		x.Add(&x, s.Mul(a[i], s.Mul(&s, &q)))
+	}
+	return x.Mod(&x, p), nil
 }
 
 func main() {
 
-	lines := util.LoadString("input")
-	departures := make([]Departure, 0)
-	i := 0
-	max := 0
-	min := 10000
+	/* Parse input. The first number can be ingored.
+	 * The second one is a coma-separated list of busses with their
+	 * departure interval
+	 */
+	fmt.Println("Input file:", inputFile)
+	lines := util.LoadString(inputFile)
+	n := make([]*big.Int, 0)
+	a := make([]*big.Int, 0)
+	i := int64(0)
 	for _, cell := range strings.Split(lines[1], ",") {
 
 		if cell == "x" {
@@ -49,94 +63,17 @@ func main() {
 			continue
 		}
 
-		num, err := strconv.Atoi(cell)
+		num, err := strconv.ParseInt(cell, 10, 64)
 		if err != nil {
 			fmt.Println("Cell is not a number", cell)
 			return
 		}
-		departures = append(departures, Departure{
-			Index: uint64(i),
-			BusID: uint64(num),
-		})
-		if num > max {
-			max = num
-		}
-
-		if num < min {
-			min = num
-		}
+		a = append(a, big.NewInt(-i))
+		n = append(n, big.NewInt(num))
 		i++
 	}
-	fmt.Println("min,max: ", min, max)
 
-	maxWorkers := runtime.GOMAXPROCS(0)
-	fmt.Println("Max workers:", maxWorkers)
-	sem := semaphore.NewWeighted(int64(maxWorkers))
-	step := uint64(1000000000)
-	timeStep := uint64(min)
-	ch := make(chan uint64, 0)
-
-	result := uint64(0)
-	for t := uint64(0); result == 0; t += step {
-
-		if sem.TryAcquire(1) {
-			go worker(t, t+step, timeStep, departures, sem, ch)
-			mtx.Lock()
-			count++
-			mtx.Unlock()
-
-			fmt.Printf(
-				"\rRunning: %d Done: %d t: %d",
-				count,
-				doneCount,
-				t,
-			)
-		}
-		select {
-		case result = <-ch:
-		case <-time.After(100 * time.Millisecond):
-		}
-	}
-
-	fmt.Println("\r>>", result, "<<")
+	fmt.Println(crt(a, n))
 	return
 
-}
-
-func worker(
-	start, end, step uint64,
-	deps []Departure,
-	sem *semaphore.Weighted,
-	resultCh chan uint64) {
-
-	defer sem.Release(1)
-	defer func() {
-		mtx.Lock()
-		count--
-		doneCount++
-		mtx.Unlock()
-	}()
-
-	for t := start; t < end; t += step {
-		if checkDepartures(t, deps) {
-			resultCh <- t
-			return
-		}
-	}
-}
-
-func checkDepartures(startTime uint64, deps []Departure) bool {
-	for i := 0; i < len(deps); i++ {
-		t := deps[i].Index
-		/*
-			fmt.Printf("(%d+%d)%%%d=%d\n",
-				t, startTime, deps[i].BusID,
-				(t+startTime)%deps[i].BusID,
-			)*/
-		if (t+startTime)%deps[i].BusID != 0 {
-			return false
-		}
-	}
-
-	return true
 }
