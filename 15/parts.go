07b7da2ca9d4def773349f6bde7824710a655513
Raphael Pour <info@raphaelpour.de>
2020-12-29 13:53:06 +0100

    15: Refactoring

--- 15/parts.go
+++ 15/parts.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"github.com/RaphaelPour/aoc2020/util"
-	"github.com/fatih/color"
 	"strconv"
 	"strings"
 )
@@ -13,20 +12,7 @@ type Spoken struct {
 }
 
 var (
-	goodColor = color.New(color.FgGreen, color.Bold)
-	badColor  = color.New(color.FgRed, color.Bold)
 	inputFile = "input"
-	solutions = map[string][]int{
-		"input":  {496, 883},
-		"input2": {436, 175594},
-		"input3": {1, 2578},
-		"input4": {10, 3544142},
-		"input5": {27, 261214},
-		"input6": {78, 6895259},
-		"input7": {438, 18},
-		"input8": {1836, 362},
-		"input9": {21, 27},
-	}
 )
 
 func main() {
@@ -43,17 +29,13 @@ func main() {
 		history2[num] = Spoken{turn: int(i + 1), preTurn: 0}
 	}
 
-	checkSolution(play(2020, history1), 1)
-	checkSolution(play(30000000, history2), 2)
-
-	return
-
+	fmt.Println(play(2020, history1))
+	fmt.Println(play(30000000, history2))
 }
 
 func play(rounds int, history map[int]Spoken) int {
-
-	next := int(0)
-	for turn := int(len(history) + 1); turn < rounds; turn++ {
+	next := 0
+	for turn := len(history) + 1; turn < rounds; turn++ {
 		if _, ok := history[next]; !ok {
 			history[next] = Spoken{turn: turn, preTurn: 0}
 			next = 0
@@ -67,20 +49,4 @@ func play(rounds int, history map[int]Spoken) int {
 	}
 
 	return next
-}
-
-func checkSolution(result int, part int) {
-
-	fmt.Printf("%d: ", part)
-
-	solution, ok := solutions[inputFile]
-	if ok {
-		if solution[part-1] == result {
-			goodColor.Println(result)
-		} else {
-			badColor.Println(result)
-		}
-	} else {
-		fmt.Println(result)
-	}
 }
